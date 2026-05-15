5e7dd3a415ac11b590f3a0f14193f33ad392daba
Conrado Fonseca <conradob@live.com>
2026-05-15 14:23:59 +0100

    Add WireGuard service with env-driven config generator

---
+++ compose/wireguard/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+: "${WG_PRIVATE_KEY:?WG_PRIVATE_KEY must be set}"
+: "${WG_PEER_PUBLIC_KEY:?WG_PEER_PUBLIC_KEY must be set}"
+: "${WG_PEER_PRESHARED_KEY:?WG_PEER_PRESHARED_KEY must be set}"
+
+mkdir -p /config/wg_confs
+
+cat > /config/wg_confs/wg0.conf <<EOF
+[Interface]
+Address    = 10.8.0.1/24
+ListenPort = 51820
+PrivateKey = ${WG_PRIVATE_KEY}
+
+[Peer]
+# home machine
+PublicKey    = ${WG_PEER_PUBLIC_KEY}
+PresharedKey = ${WG_PEER_PRESHARED_KEY}
+AllowedIPs   = 10.8.0.2/32
+EOF
+
+chmod 600 /config/wg_confs/wg0.conf
+echo "wg0.conf generated"
